fb6a051e3f330775212d046cbd9638be1dec5ba3
Carlos Segarra <carlossegarragonzalez@gmail.com>
2019-09-04 17:20:23 +0200

    Modified the TA

--- cache_benchmark/ta/cache_benchmarking_ta.c
+++ cache_benchmark/ta/cache_benchmarking_ta.c
@@ -38,12 +38,190 @@
 #define NUM_TESTS               2 //100
 #define TA_AES_KEY_SIZE         32
 #define TA_MQTTZ_CLI_ID_SZ      12
-/*
- * Read Raw Object from Secure Storage within TA
- *
- * This method reads an object from Secure Storage but is always invoked
- * from within a TA. Hence why we don't check the parameter types.
- */
+#define TOTAL_ELEMENTS          1024
+// To change every experiment
+#define CACHE_SIZE              1024
+
+typedef struct Node {
+    char *id;
+    char *data;
+    struct Node *next;
+    struct Node *prev;
+} Node;
+
+typedef struct Queue {
+    Node *first;
+    Node *last;
+    int size;
+    int max_size;
+} Queue;
+
+typedef struct Hash {
+    int capacity;
+    Node **array;
+} Hash;
+
+static Node* init_node(char *id, char *data)
+{
+    Node *tmp = (Node *) TEE_Malloc(sizeof(Node), 0);
+    tmp->id = (char *) TEE_Malloc(sizeof(char) * (OBJ_ID_SIZE + 1), 0);
+    strncpy(tmp->id, id, OBJ_ID_SIZE);
+    tmp->id[OBJ_ID_SIZE] = '\0';
+    tmp->data = (char *) TEE_Malloc(sizeof(char) * (KEY_SIZE + 1), 0);
+    strncpy(tmp->data, data, KEY_SIZE);
+    tmp->data[KEY_SIZE] = '\0';
+    tmp->prev = tmp->next = NULL;
+    return tmp;
+}
+
+static int free_node(Node *node)
+{
+    TEE_Free((void *) node->id);
+    TEE_Free((void *) node->data);
+    TEE_Free((void *) node); 
+    return 0;
+}
+
+static Queue* init_queue(int max_size)
+{
+    Queue *queue = (Queue *) TEE_Malloc(sizeof(Queue), 0);
+    queue->first = NULL;
+    queue->last = NULL;
+    queue->size = 0;
+    queue->max_size = max_size;
+    return queue;
+}
+
+static int free_queue(Queue *queue)
+{
+    TEE_Free((void *) queue->first);
+    TEE_Free((void *) queue->last);
+    TEE_Free((void *) queue);
+    return 0;
+}
+
+static Hash* init_hash(int capacity)
+{
+    Hash *hash = (Hash *) TEE_Malloc(sizeof(Hash), 0);
+    hash->capacity = capacity;
+    hash->array = (Node **) TEE_Malloc(sizeof(Node*) * hash->capacity, 0);
+    unsigned int i;
+    for (i = 0; i < hash->capacity; i++)
+        hash->array[i] = NULL;
+    return hash;
+}
+
+int free_hash(Hash *hash)
+{
+    TEE_Free((void *) hash->array);
+    TEE_Free((void *) hash);
+    return 0;
+}
+
+Node* queue_pop(Queue *queue)
+{
+    // FIFO policy for the time being
+    if (queue->size == 0)
+        return NULL;
+    Node *old_last = queue->last;
+    queue->last = queue->last->prev;
+    queue->last->next = NULL;
+    queue->size -= 1;
+    return old_last;
+}
+
+int queue_push(Queue *queue, Node *node)
+{
+    // FIFO policy for the time being
+    if (queue->size == CACHE_SIZE)
+        queue_pop(queue);
+    if (queue->first != NULL)
+    {
+        queue->first->prev = node;
+        node->next = queue->first;
+    }
+    else
+    {
+        queue->last = node;
+    }
+    queue->first = node;
+    queue->size += 1;
+    return 0;
+}
+
+int queue_to_front(Queue *queue, Node *node)
+{
+    if (queue->first == node)
+        return 0;
+    if (queue->last == node)
+        queue->last == node->prev;
+    else
+        node->next->prev = node->prev;
+    node->prev->next = node->next;
+    node->prev = NULL;
+    node->next = queue->first;
+    queue->first->prev = node;
+    queue->first = node;
+    return 0;
+}
+
+Node* cache_query(Hash *hash, Queue *queue, char *obj_id)
+{
+    int page = atoi(obj_id) % hash->capacity;
+    Node *reqPage = hash->array[page];
+    if (reqPage == NULL)
+    {
+        // Cache Miss
+        // Load from Secure Storage FIXME FIXME TODO
+        // We do this instead for testing!
+        reqPage = init_node(obj_id, "1111111111111111");
+        if (queue->size == CACHE_SIZE)
+        {
+            Node *tmp = queue_pop(queue);
+            int tmp_index = atoi(tmp->id) % hash->capacity;
+            hash->array[tmp_index] = NULL;
+            free_node(tmp);
+        }
+        queue_push(queue, reqPage);
+        hash->array[page] = reqPage;
+        return reqPage;
+    }
+    queue_to_front(queue, reqPage);
+    return reqPage;
+}
+
+
+void print_queue_status(Queue *queue)
+{
+    printf("-----------------------\n");
+    printf("Current Queue Status:\n\t- Queue Size: %i\n\t- Elements:\n",
+            queue->size);
+    int i;
+    Node *current = queue->first;
+    for (i = 0; i < queue->size; i++)
+    {
+        printf("\t\t%i -> %s\n", i, current->data);
+        current = current->next;
+    }
+    printf("-----------------------\n");
+}
+
+void print_cache_status(Hash *hash)
+{
+    printf("-----------------------\n");
+    printf("Current Hash Status:\n\t- Table Size: %i\n", hash->capacity);
+    printf("\t- Cache Size: %i\n\t- Elements:\n", CACHE_SIZE);
+    unsigned int i;
+    for (i = 0; i < hash->capacity; i++)
+    {
+        if (hash->array[i] != NULL)
+            printf("\t\t%i -> %s\n", i, hash->array[i]->data);
+        else
+            printf("\t\t%i -> \n", i);
+    }
+    printf("-----------------------\n");
+}
+
 static TEE_Result read_raw_object(char *cli_id, size_t cli_id_size, char *data,
         size_t data_sz)
 {
@@ -149,12 +327,28 @@ static TEE_Result cache_benchmarking(void *session, uint32_t param_types,
     TEE_Time t1, t2, t_aux;
     char fke_key[TA_AES_KEY_SIZE + 1] = "11111111111111111111111111111111";
     uint32_t exp_param_types = TEE_PARAM_TYPES(
-            TEE_PARAM_TYPE_MEMREF_INPUT,
-            TEE_PARAM_TYPE_MEMREF_INOUT,
-            TEE_PARAM_TYPE_MEMREF_INOUT,
-            TEE_PARAM_TYPE_VALUE_INPUT);
+            TEE_PARAM_TYPE_NONE,
+            TEE_PARAM_TYPE_NONE,
+            TEE_PARAM_TYPE_NONE,
+            TEE_PARAM_TYPE_NONE);
     if (param_types != exp_param_types)
         return TEE_ERROR_BAD_PARAMETERS;
+    Queue *queue = init_queue(CACHE_SIZE);
+    Hash *hash = init_hash(TOTAL_ELEMENTS);
+    printf("Initialized Queue!\n");
+    print_cache_status(hash);
+    cache_query(hash, queue, "000000000000");
+    print_cache_status(hash);
+    cache_query(hash, queue, "000000000001");
+    print_cache_status(hash);
+    cache_query(hash, queue, "000000000002");
+    print_cache_status(hash);
+    cache_query(hash, queue, "000000000003");
+    print_cache_status(hash);
+    free_queue(q);
+    free_hash(hash);
+    res = TEE_SUCCESS;
+    return res;
 }
 
 
