0f4f0e82e32a97d834f14cad677ffa0cdf8d7d43
Carlos Segarra <carlossegarragonzalez@gmail.com>
2019-12-08 18:19:11 +0100

    jj

--- hot_cache/ta/user_ta_header_defines.h
+++ hot_cache/ta/user_ta_header_defines.h
@@ -37,8 +37,8 @@
 #define TA_UUID				TA_HOT_CACHE_UUID
 
 #define TA_FLAGS			(TA_FLAG_EXEC_DDR | TA_FLAG_SINGLE_INSTANCE)
-#define TA_STACK_SIZE			(2 * 1024)
-#define TA_DATA_SIZE			(32 * 1024)
+#define TA_STACK_SIZE			(4 * 1024)
+#define TA_DATA_SIZE			(64 * 1024)
 
 #define TA_CURRENT_TA_EXT_PROPERTIES \
     { "gp.ta.description", USER_TA_PROP_TYPE_STRING, \
