567212b0f131b14b1b778413d7c9f7db94fac6a7
Carlos Segarra <carlossegarragonzalez@gmail.com>
2020-02-02 19:16:12 +0100

    multithreaded TCP server implemented

--- threaded-socket/host/server/commmand.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-read MESSAGE
-echo $MESSAGE
--- threaded-socket/host/server/tcp_server.py
+++ threaded-socket/host/server/tcp_server.py
@@ -1,24 +1,31 @@
 import socket, threading
 class ClientThread(threading.Thread):
-    def __init__(self,clientAddress,clientsocket):
+
+    def __init__(self, address, socket):
         threading.Thread.__init__(self)
-        self.csocket = clientsocket
-        print ("New connection added: ", clientAddress)
+        self.csocket = socket
+        self.address = address
+        print ("New connection added: ", address)
+
     def run(self):
-        print ("Connection from : ", clientAddress)
-        #self.csocket.send(bytes("Hi, This is from Server..",'utf-8'))
-        msg = ''
+        print ("Connection from : ", self.address)
         while True:
-            data = self.csocket.recv(2048)
-            msg = data.decode()
-            if msg=='bye':
-              break
-            print ("from client", msg)
-            self.csocket.send(bytes(msg,'UTF-8'))
-        print ("Client at ", clientAddress , " disconnected...")
+            try:
+                data = self.csocket.recv(2048)
+                if data:
+                    print('received {!r} from {}'.format(data, self.address))
+                else:
+                    print("client disconnected")
+                    self.csocket.close()
+                    break
+            except OSError:
+                print("client disconnected")
+                self.csocket.close()
+                break
 
 LOCALHOST = "127.0.0.1"
-PORT = 9999
+PORT = 9990
+data_size = 1 * 1024
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.bind((LOCALHOST, PORT))
