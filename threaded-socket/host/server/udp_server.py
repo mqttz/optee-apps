1ad7cb66deed9205bd59f303fa56da0949c2109e
Carlos Segarra <carlossegarragonzalez@gmail.com>
2020-02-02 19:35:06 +0100

    UDP multi-threaded server finished

--- threaded-socket/host/server/udp_server.py
+++ threaded-socket/host/server/udp_server.py
@@ -7,7 +7,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 data_size = 1 * 1024
 
 # Bind the socket to the port
-server_address = ('127.0.0.1', 9998)
+server_address = ('127.0.0.1', 9980)
 print('starting up UDP server on {} port {}'.format(*server_address))
 sock.bind(server_address)
 
@@ -16,4 +16,4 @@ while True:
     print('waiting for a UDP connection')
     data, client_address = sock.recvfrom(data_size + 1)
     print('UDP connection from', client_address)
-    print('received {!r}'.format(data))
+    print('received {!r} from {}'.format(data, client_address))
